d6733837bc3e1b2521dfb7e6910176a6431511c5
ciclo6moviles2024idat <juankuga@Juans-MacBook-Pro.local>
2024-07-08 21:56:23 -0500

    API Rest Get viewModel

--- app/src/main/java/pe/idat/appmvvmapirest/posts/data/network/service/PostService.kt
+++ app/src/main/java/pe/idat/appmvvmapirest/posts/data/network/service/PostService.kt
@@ -1,4 +1,18 @@
 package pe.idat.appmvvmapirest.posts.data.network.service
 
-class PostService {
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import pe.idat.appmvvmapirest.posts.data.network.response.PostResponse
+import pe.idat.appmvvmapirest.posts.data.network.retrofitclient.PostClient
+import javax.inject.Inject
+
+class PostService @Inject constructor(private val postClient: PostClient) {
+
+    suspend fun getPosts(): List<PostResponse>{
+        return withContext(Dispatchers.IO){
+            val response = postClient.getPosts()
+            response.body()!!
+        }
+    }
+
 }
--- app/src/main/java/pe/idat/appmvvmapirest/posts/data/repository/PostRepository.kt
+++ app/src/main/java/pe/idat/appmvvmapirest/posts/data/repository/PostRepository.kt
@@ -1,4 +1,12 @@
 package pe.idat.appmvvmapirest.posts.data.repository
 
-class PostRepository {
+import pe.idat.appmvvmapirest.posts.data.network.response.PostResponse
+import pe.idat.appmvvmapirest.posts.data.network.service.PostService
+import javax.inject.Inject
+
+class PostRepository @Inject constructor(private val api: PostService) {
+
+    suspend fun getPost(): List<PostResponse>{
+        return api.getPosts()
+    }
 }
